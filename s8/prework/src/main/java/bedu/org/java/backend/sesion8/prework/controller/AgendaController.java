2fc560490506b3749b18691ea681fe76e8550e1b
Francisco Santiago de la Cruz <fcosantiagoc@gmail.com>
2023-03-26 10:22:57 -0600

    Fix registro de usuarios, control de errores formulario

--- s8/prework/src/main/java/bedu/org/java/backend/sesion8/prework/controller/AgendaController.java
+++ s8/prework/src/main/java/bedu/org/java/backend/sesion8/prework/controller/AgendaController.java
@@ -4,6 +4,7 @@ import bedu.org.java.backend.sesion8.prework.service.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,10 +31,10 @@ public class AgendaController {
     }
 
     @PostMapping("/registro")
-    public ModelAndView registra(@Valid Persona persona) {
-
-        agendaService.guardaPersona(persona);
-
+    public ModelAndView registra(@Valid Persona persona, Errors errors) {
+        if(!errors.hasErrors()) {
+            agendaService.guardaPersona(persona);
+        }
         ModelAndView mav = new ModelAndView("index");
         mav.addObject("listaPersonas", agendaService.getPersonas());
         return mav;
